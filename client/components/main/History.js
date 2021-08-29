78359caaa5bc35496e2ad4fba0b74438c7b38eec
YentelDeHauwere <yentel.dh@hotmail.be>
2021-08-29 21:07:56 +0200

    fixes history

--- client/components/main/History.js
+++ client/components/main/History.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image, ScrollView, StyleSheet } from 'react-native'
 import axios from 'axios'
 import { API_URL } from '@env'
 import SimpleDateTime  from 'react-simple-timestamp-to-date';
@@ -7,6 +7,17 @@ import SimpleDateTime  from 'react-simple-timestamp-to-date';
 export default function History() {
 	const [ detectedAnimals, setDetectedAnimals ] = useState([])
 	const [ numberPeople, setNumberPeople] = useState()
+	const [ dummy, setDummy ] = useState([
+		{
+			animalType: 'bird',
+			timeStamp: '1630258927'
+		}, 
+		{
+			animalType: 'dog',
+			timeStamp: '1630258943'
+		}
+	])
+	
 	const dataUrl = `${API_URL}api`
 
 	useEffect(() => {
@@ -19,30 +30,57 @@ export default function History() {
 			const response = await axios.get(dataUrl);
 			setDetectedAnimals(response.data.animalDetections.animals)
 			setNumberPeople(response.data.numPerson)
+			console.log(response.data)
+
 
 		} catch (err) {
 			console.log(err.message)
 		  	return err.message
 		}
 	};
-	
+
 	useEffect(() => {
 		console.log(detectedAnimals)
 	}, [detectedAnimals])
-
-	useEffect(() => {
-		console.log(numberPeople)
-	}, [numberPeople])
 	
 	return (
-		<View>
-			{ detectedAnimals.map((animal, key)=>(
-				<View key={key}>
-					<Text> { animal.animalType } </Text>
-					<SimpleDateTime dateFormat="DMY" dateSeparator="/"  timeSeparator=":">{animal.timeStamp}</SimpleDateTime>
-				</View>
-				)
-			)}
+		<View style={styles.container}>
+			<ScrollView>
+				{ detectedAnimals.map((animal, key)=>(
+					<View key={key}>
+						<Image style={styles.image} source={{uri:"https://images.unsplash.com/photo-1526045612212-70caf35c14df"}}/>
+						<Text style={styles.text}>{ animal.animalType }</Text>
+						<Text style={styles.date} >
+							<SimpleDateTime 
+								dateFormat="DMY" 
+								dateSeparator="." 
+								timeSeparator=":">
+									{animal.timeStamp}
+							</SimpleDateTime>
+						</Text>
+					</View>
+					)
+				)}
+			</ScrollView>
 		</View>
 	)
 }
+
+const styles = StyleSheet.create({
+	container: {
+		paddingHorizontal: 20,
+	},
+	image: {
+	  resizeMode: 'cover', 
+	  width: 200,
+	  height: 100,
+	},
+	text: {
+	  textTransform: 'uppercase',
+	  fontSize: 18,
+	},
+	date: {
+	  textTransform: 'uppercase',
+	  fontSize: 10,
+	}
+  });
